bc96eca117555b9fad39705dc9729168a42de040
Fauziah <uzziifz7@gmail.com>
2024-09-02 16:04:55 +0800

    Day1

---
+++ Day01.java
@@ -0,0 +1,6 @@
+public class day_one_coding {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Fauziah from informatics class D.");
+        System.out.println("Ini adalah projek pertamaku dalam program 100 hari coding.");
+    }
+}
