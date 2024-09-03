ecf42319ec2b218afb4e80d5a37118fb95e6de7a
Fauziah <uzziifz7@gmail.com>
2024-09-03 18:04:37 +0800

    Day2
    Membuat variable

---
+++ Day2.java
@@ -0,0 +1,31 @@
+public class day_two_coding {
+    public static void main(String[] args) {
+        System.out.println("Belajar jenis variable dan tipe data");
+        // Membuat variable
+        // Tipe data String
+        String nama = "Fauziah";
+        String alamat = "jln.A.Manginda,kec.Pitumpanua,kab.Wajo,Sulawesi Selatan";
+        
+        // Tipe data int
+        int umur = 18;
+        
+        // Tipe data char
+        char jenis_kelamin = 'P';
+        
+        // Tipe data double
+        double nilai_rapor = 91.88;
+        
+        // Tipe data boolean
+        boolean status_lulus = true;
+        
+        
+        //menampilkan variable
+        System.out.println("Nama :" + nama);
+        System.out.println("Alamat :" + alamat);
+        System.out.println("Umur :" + umur);
+        System.out.println("Jenis kelamin :" + jenis_kelamin);
+        System.out.println("Nilai rapor :" + nilai_rapor);
+        System.out.println("Status lulus :" + status_lulus);
+    }
+    
+}
