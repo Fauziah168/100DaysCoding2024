59d637e7161f69a848a0c40a689196514206c758
Fauziah <uzziifz7@gmail.com>
2024-09-04 17:05:01 +0800

    Day03
    Mengkreasikan sout

---
+++ Day03.java
@@ -0,0 +1,27 @@
+public class day_three_coding {
+    
+    public static void main(String[] args) {
+        // menampilkan kata yang berada dalam kurung
+        System.out.println("Hello, World!");
+        System.out.println();
+        
+        //menampilkan kata dengan warna yang berbeda
+        System.out.println("\033[0;31mHello, World!");
+        System.out.println("\033[0;32mHello, World!");
+        System.out.println("\033[0;33mHello, World!");
+        System.out.println("\033[0;30m");
+        
+        // menampikan angka yang berada dalam kurung
+        System.out.println("7 + 9");
+        System.out.println("20 / 5");
+        System.out.println("2 * 6");
+        System.out.println();
+        
+        // mengoperasikan angka yang berada dalam kurung
+        System.out.println(7 + 9);
+        System.out.println(20 / 5);
+        System.out.println(2 * 6);
+        
+    }
+    
+}
