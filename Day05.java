ebe8635455e0aa265ac068c4be888f0534597548
Fauziah <uzziifz7@gmail.com>
2024-09-06 12:27:19 +0800

    Day5
    Penggunaan komentar

---
+++ Day05.java
@@ -0,0 +1,17 @@
+public class day_five_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Belajar menggunakan komentar");
+        System.out.println("Komentar terbagi menjadi dua jenis");
+        System.out.println("yaitu komentar satu baris");
+        //contoh komentar satu baris
+        System.out.println("Dan komentar beberapa baris");
+        /* ini adalah contoh penggunaan 
+         * komentar beberapa baris
+         * komentar 1
+         * komentar 2
+         * komentar 3
+         */
+    }
+    
+}
