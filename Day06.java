ef119cd439d7077afde5dd0d5dee5cd6d7782999
Fauziah <uzziifz7@gmail.com>
2024-09-07 15:57:12 +0800

    Day6
    Membuat variable deklarasi

---
+++ Day06.java
@@ -0,0 +1,13 @@
+public class dqy_six_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("membuat variable : deklarasi");
+        int umur;
+        char jenis_kelamin;
+        String nama,alamat,no_telpon;
+        float tinggi_badan;
+        double nilai;
+        boolean status_lulus;
+    }
+    
+}
