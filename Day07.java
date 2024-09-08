3bdcf561e4db18f39ae96bac07b302e2c46f1d74
Fauziah <uzziifz7@gmail.com>
2024-09-08 20:06:56 +0800

    Day7
    Variable : inisialisasi

---
+++ Day07.java
@@ -0,0 +1,24 @@
+public class day_seven_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("membuat variable : inisialisasi");
+        int umur = 18;
+        char jenis_kelamin = 'P';
+        String nama,alamat,no_telpon;
+        nama = "Fauziah";
+        alamat = "wajo,sulawesi selatan";
+        no_telpon = "087735559664";
+        float tinggi_badan = 150.562318f;
+        double nilai = 91.87;
+        boolean status_lulus = true;
+        System.out.println("Umur" + umur);
+        System.out.println("Jenis kelamin" + jenis_kelamin);
+        System.out.println("Nama" + nama);
+        System.out.println("Alamat" + alamat);
+        System.out.println("No telpon" + no_telpon);
+        System.out.println("Tinggi badan" + tinggi_badan);
+        System.out.println("Nilai" + nilai);
+        System.out.println("Status lulus" + status_lulus);
+    }
+    
+}
