c47f14cbd3e8457813a8a93cb2d2d65d7986b033
Fauziah <uzziifz7@gmail.com>
2024-09-11 03:43:23 +0800

    Day9
    Membuat final variable

---
+++ Day09.java
@@ -0,0 +1,25 @@
+public class day_nine_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("membuat variable : final");
+        final int umur = 18;
+        final char jenis_kelamin = 'P';
+        final String nama,alamat,no_telpon;
+        nama = "Fauziah";
+        alamat = "wajo,sulawesi selatan";
+        no_telpon = "087735559664";
+        final float tinggi_badan = 150.562318f;
+        final double nilai = 91.87;
+        final boolean status_lulus = true;
+        
+        System.out.println("Umur" + umur);
+        System.out.println("Jenis kelamin" + jenis_kelamin);
+        System.out.println("Nama" + nama);
+        System.out.println("Alamat" + alamat);
+        System.out.println("No telpon" + no_telpon);
+        System.out.println("Tinggi badan" + tinggi_badan);
+        System.out.println("Nilai" + nilai);
+        System.out.println("Status lulus" + status_lulus);
+    }
+    
+}
