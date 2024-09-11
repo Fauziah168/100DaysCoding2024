ec40a929d406f97d9a99d52ffb7b0b33f9ca6557
Fauziah <uzziifz7@gmail.com>
2024-09-11 13:45:17 +0800

    Day10
    Penggunaan byte

---
+++ Day10.java
@@ -0,0 +1,16 @@
+public class day_ten_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data byte");
+       /*byte di gunakan untuk bilangan bulat kecil
+         mulai dari -128 sampai 127*/
+        
+        byte angka = -128;
+        byte angka1 = 127;
+        System.out.println("angka" + angka);
+        System.out.println("angka1" + angka1);
+        
+      //codingan akan error jika angka melebihi ketentuan   
+    }
+    
+}
