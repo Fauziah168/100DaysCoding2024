f978213a5b3833404d1f4dd6cef6f37432d28bd7
Fauziah <uzziifz7@gmail.com>
2024-09-12 21:35:15 +0800

    Tipe data : short

---
+++ Day11.java
@@ -0,0 +1,21 @@
+public class day_eleven_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data short");
+        /* tipe data short gi gunakan untuk menyimpan 
+           bilangan bulat dengan rentang nilai mulai
+           dari -32.768 hingga 32.767*/
+        
+        short bilangan = -32768;
+        short bilangan1 = 32767;
+        System.out.println("bilangan" + bilangan);
+        System.out.println("bilabgan1" + bilangan1);
+        
+        /*jika angka yang di masukan lebih dari
+          rentang nilai yang telah di tentukan 
+          maka hasil codingan akan error*/
+        
+        
+    }
+    
+}
