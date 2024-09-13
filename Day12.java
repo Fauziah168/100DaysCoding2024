30d35c96ca7c9fcfa44c8f93a942da834b1c7ffa
Fauziah <uzziifz7@gmail.com>
2024-09-13 19:46:24 +0800

    Tipe data : int
    Penggunaan tipe data integer

---
+++ Day12.java
@@ -0,0 +1,13 @@
+public class day_twelve_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data : integer");
+       /* tipe data integer di gunakan untuk angka bulat
+          baik itu negatif atau pun positif*/
+        int umur = 18;
+        int suhu = -48;
+        System.out.println("umur :" + umur);
+        System.out.println("Suhu :" + suhu + "°c");
+    }
+    
+}
