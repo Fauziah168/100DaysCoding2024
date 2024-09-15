bbf4f7a0bf3e3ed35c0311fb666b0a89af45c532
Fauziah <uzziifz7@gmail.com>
2024-09-15 23:01:58 +0800

    Tipe data : long

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class day_fourteen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data long");
+        /* tipe data long di gunakan untuk menyimpan bilangan
+           bulat dengan nilai besar, mulai dari  -9,223,372,036,854,775,808
+           sampai 9,223,372,036,854,775,807 */
+        long angka = 123456789;
+        long angka1 = -123456789;
+        System.out.println("angka : " + angka ); 
+        System.out.println("angka1 : " + angka1 );
+    }
+    
+}
