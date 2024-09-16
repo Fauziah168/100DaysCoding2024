1ba79e220d23e6d387ad5894411f094274dbf4e5
Fauziah <uzziifz7@gmail.com>
2024-09-16 19:17:32 +0800

    Tipe data : double

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class day_fifteen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data double");
+        /* tipe data double adalah tipe data yang di gunakan
+           untuk menyimpan bilangan desimal(pecahan)*/
+        double nilai = 98.76;
+        double suhu = -36.67;
+        System.out.println("Nilai :" + nilai);
+        System.out.println("Suhu :" + suhu + "°c");
+    }
+    
+}
