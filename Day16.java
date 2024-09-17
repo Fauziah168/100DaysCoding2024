6b6a04baaa5774190897aaa68b32842d45530e67
Fauziah <uzziifz7@gmail.com>
2024-09-17 19:51:30 +0800

    Tipe data : float

---
+++ Day16.java
@@ -0,0 +1,13 @@
+public class day_sixteen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipa data float");
+        /* Tipe data float digunakan untuk menyimpan 
+           bilangan desimal/pecahan*/
+        float nilai = 88.838262f;
+        float nilai1 = 97.34872f;
+        System.out.println("Nilai pertama :" + nilai);
+        System.out.println("Nilai kedua :" + nilai1);        
+    }
+    
+}
