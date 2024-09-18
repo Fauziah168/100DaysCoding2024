d64db5e68c6f16837ea5e6528d238404d9a4d998
Fauziah <uzziifz7@gmail.com>
2024-09-18 18:58:46 +0800

    Tipe data : char

---
+++ Day17.java
@@ -0,0 +1,15 @@
+public class day_seventeen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data char");
+        /* tipe data char di gunakan untuk menyimpan satu
+         karakter seperti huruf, angka, atau simbol khusus*/
+        char var1 = 'A';
+        char var2 = '8';
+        char var3 = '!';
+        System.out.println("Huruf : " + var1);
+        System.out.println("Angka : " + var2);
+        System.out.println("Simbol :" + var3);
+    }
+    
+}
