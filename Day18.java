da06922690e3e950b90ba2f4f1c40f5c7af03f23
Fauziah <uzziifz7@gmail.com>
2024-09-19 18:12:08 +0800

    Tipe data : boolean

---
+++ Day18.java
@@ -0,0 +1,14 @@
+public class day_eighteen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipe data boolean");
+        /* tipe data boolean adalah tipe data yang hanya
+           memiliki dua nilai yaitu true atau false*/
+        boolean pernyataan1,pernyataan2;
+        pernyataan1 = true;
+        pernyataan2 = false;
+        System.out.println("pernyataan pertama : " + pernyataan1);
+        System.out.println("pernyataan kedua : " + pernyataan2);
+    }
+    
+}
