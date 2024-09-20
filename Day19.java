68f2e32d781ec48b61f1a67e0f3f93ec79c7ee48
Fauziah <uzziifz7@gmail.com>
2024-09-20 19:14:27 +0800

    Tipe data reference : string

---
+++ Day19.java
@@ -0,0 +1,12 @@
+public class day_nineteen_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan tipa data reference string");
+        String nama;
+        nama = "Fauziah";
+        System.out.println("halo perkanalkan nama saya " + nama);
+        
+             
+    }
+    
+}
