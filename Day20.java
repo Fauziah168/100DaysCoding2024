4844a7597b02e25ccb4a2cd064656ba4d0470f1e
Fauziah <uzziifz7@gmail.com>
2024-09-21 22:12:47 +0800

    Tipe data reference : Byte

---
+++ Day20.java
@@ -0,0 +1,12 @@
+public class day_twenty_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data reference byte");
+        Byte var1 = 75;
+        Byte var2 = 20;
+        
+        System.out.println("variable pertama : " + var1);
+        System.out.println("variable kedua : " + var2);
+    }
+    
+}
