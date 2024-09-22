2d866fa94013fa2e1dba1d3af6b65a27ea3fce4a
Fauziah <uzziifz7@gmail.com>
2024-09-22 20:45:11 +0800

    Tipe data reference :  Short

---
+++ Day21.java
@@ -0,0 +1,12 @@
+public class day_twentyone_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data reference short");
+        Short var1, var2;
+         var1 = 4572;
+        var2 = -1789;
+        System.out.println("Angka pertama : " + var1);
+        System.out.println("Angka kedua : " + var2);
+    }
+    
+}
