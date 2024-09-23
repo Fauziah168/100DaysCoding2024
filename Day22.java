817557f009b4b99164695c75608a4a5079ccc982
Fauziah <uzziifz7@gmail.com>
2024-09-23 19:59:41 +0800

    Tipe data reference : Integer

---
+++ Day22.java
@@ -0,0 +1,11 @@
+public class day_twentytwo_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data reference integer");
+        Integer angka1 = 765;
+        Integer angka2 = 80;
+        Integer hasil =angka1 / angka2;
+        System.out.println("hasil dari pembagian angka pertama dan kedua adalah " + hasil);
+    }
+    
+}
