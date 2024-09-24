e3643d56adf257caf6b7800dde2b973150362ba1
Fauziah <uzziifz7@gmail.com>
2024-09-24 19:00:38 +0800

    Tipe data reference : Long

---
+++ Day23.java
@@ -0,0 +1,12 @@
+public class day_twentythree_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data reference long");
+        Long bilangan1, bilangan2, hasil;
+        bilangan1 = 7535L;
+        bilangan2 = 1234L;
+        hasil = bilangan1 - bilangan2;
+        System.out.println("Hasil pengurangan dari bilangan pertama dan kedua adalah " + hasil);
+    }
+    
+}
