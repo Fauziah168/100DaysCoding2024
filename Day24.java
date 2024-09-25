c8baaafd65daefaeafacfacd93c8130dec9fdc5a
Fauziah <uzziifz7@gmail.com>
2024-09-25 19:50:56 +0800

    Tipe data reference : Float

---
+++ Day24.java
@@ -0,0 +1,12 @@
+public class day_twentyfour_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data reference float");
+        Float var1 = 7.0f;
+        Float var2 = 3.0f;
+        Float hasil = var1 / var2 ;
+        System.out.println("hasil bagi : " + hasil);
+        
+    }
+    
+}
