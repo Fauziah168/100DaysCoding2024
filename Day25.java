88462690d3586d5e0d22059381570ef4c1dd6f42
Fauziah <uzziifz7@gmail.com>
2024-09-26 19:43:39 +0800

    Tipe data reference : Double

---
+++ Day25.java
@@ -0,0 +1,15 @@
+public class day_twentyfive_coding {
+    public static void main(String[] args) {
+        System.out.println("Pengunaan tipe data referance double");
+        Double nilai1 = 78.67;
+        Double nilai2 = 89.50;
+        Double nilai3 = 90.88;
+        Double jumlah = nilai1 + nilai2 + nilai3;
+        System.out.println("Nilai Matematika : " + nilai1);
+        System.out.println("Nilai Bhs.inggris : " + nilai2);
+        System.out.println("Nilai Bhs.Indonesia : " + nilai3);
+        System.out.println("Jumlah seluruh nilai :" + jumlah);
+        System.out.println("Rata-rata nilai : " + (jumlah / 3));
+    }
+    
+}
