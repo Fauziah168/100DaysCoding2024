da07653bd5881aa865a7fbb479d9600c5f0dfad4
Fauziah <uzziifz7@gmail.com>
2024-09-27 23:16:15 +0800

    Tipe data reference : Character

---
+++ Day26.java
@@ -0,0 +1,14 @@
+public class day_Twentysix_coding {
+
+         public static void main(String [] args) {.                     System.out.println("Penggunaan tipe data reference character");
+         Character var1, var2, var3;
+          var1 = 'A';
+          var2 = '$';
+          var3 = '!';
+          System.out.println("Variable pertama : " + var1);
+          System.out.println("Variable kedua : " + var2);
+           System.out.println("Variable ketiga : " + var3);
+
+}
+
+}
