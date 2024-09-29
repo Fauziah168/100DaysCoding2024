5368b20951cef87eeb1bd9ff21b6396f9509dea0
Fauziah <uzziifz7@gmail.com>
2024-09-29 18:59:48 +0800

    Tipe data reference : Boolean

---
+++ Day28.java
@@ -0,0 +1,12 @@
+public class day_twentyeight_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data referance boolean");
+        Boolean pernyataan1 = true;
+        Boolean pernyataan2 = Boolean.FALSE;
+        
+        System.out.println("Pernyataan pertama : " + pernyataan1);
+        System.out.println("Pernyataan kedua : " + pernyataan2);
+    }
+    
+}
