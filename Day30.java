244e99ad0de014b3472a0eb76cb5dcda6103559e
Fauziah <uzziifz7@gmail.com>
2024-10-01 20:49:44 +0800

    Tipe data reference : BigDecimal

---
+++ Day30.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+public class day_thirty_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Penggunaan tipe data referance BigDecimal");
+        BigDecimal nilai1 = new BigDecimal("123.456789");
+        BigDecimal nilai2 = new BigDecimal("12.248");
+        
+        System.out.println("awal bulan : " + nilai1);
+        System.out.println("akhir bulan : " + nilai2);
+    }
+    
+}
