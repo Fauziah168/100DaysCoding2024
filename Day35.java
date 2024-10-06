6ef7e494ba1330821532c9a4a15f8ab71a4f3fee
Fauziah <uzziifz7@gmail.com>
2024-10-06 22:07:18 +0800

    Operator aritmatika : Penjumlahan & Pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+public class day_thirtyfive_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("operator aritmatika");
+        int angka1 = 45;
+        int angka2 = 7;
+        
+        //operator aritmatika penjumlahan
+        System.out.println("Penjumlahan : " + (angka1 + angka2));
+        
+        //operator aritmatika pengurangan
+        System.out.println("Pengurangan : " + (angka1 - angka2));
+    }
+    
+}
