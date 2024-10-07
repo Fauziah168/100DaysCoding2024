f64bde94c56bdb03b3cbb9c7ca46a7864c1560d7
Fauziah <uzziifz7@gmail.com>
2024-10-07 16:28:18 +0800

    Operator aritmatika : Perkalian, Pembagian, dan Modulo

---
+++ Day36.java
@@ -0,0 +1,21 @@
+public class day_thirtysix_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator aritmatika");
+        //operator perkalian (*)
+        byte a = 38;
+        byte b = 5;
+        System.out.println("Hasil perkalian : " + (a * b));
+        
+        //operator pembagian (/)
+        long c = 12500;
+        short d = 500;
+        System.out.println("Hasil pembagian : " + (c / d));
+        
+        //operator modulo (%)
+        float e = 80.45f;
+        double f = 6.20;
+        System.out.printf("Hasil modulus : %.2f",(e % f));
+    }
+    
+}
