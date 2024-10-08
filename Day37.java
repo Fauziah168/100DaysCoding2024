356496ec4c015da81aaf8e64b084e30a65e7e6ad
Fauziah <uzziifz7@gmail.com>
2024-10-08 20:13:21 +0800

    Operator Penugasan : Pengisian Nilai, Pengisian dan Penjumlahan, Pengisian dan Pengurangan

---
+++ Day37.java
@@ -0,0 +1,19 @@
+public class day_thirtyseven_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator penugasan");
+        int tabungan;
+        // pengisian nilai (=)
+        tabungan = 20000;
+        System.out.println("Isi tabunganku :" + tabungan);
+        
+        // pengisian dan penjumlahan (+=)
+        tabungan += 2000;
+        System.out.println("Isi tabunganku setelah ditambah :" + tabungan);
+        
+        // pengisian dan pengurangan (-=)
+        tabungan -= 12000;
+        System.out.println("Sisa tabungan ku setelah diambil :" + tabungan);
+    }
+    
+}
