9639d63a394c6ad70591c88e7220d706804765e5
Fauziah <uzziifz7@gmail.com>
2024-10-09 19:44:02 +0800

    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan Pembagian

---
+++ Day38.java
@@ -0,0 +1,17 @@
+public class day_thirtyeight_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator penugasan");
+        double nilai = 90.50;
+        System.out.println("Nilai awal : " + nilai);
+        
+        // pengisian dan perkalian
+        nilai *= 5;
+        System.out.println("Nilai setelah di kali : " + nilai);
+        
+        // pengisian dan pembagian
+        nilai /= 2;
+        System.out.println("Nilai setelah di bagi : " + nilai);
+    }
+    
+}
