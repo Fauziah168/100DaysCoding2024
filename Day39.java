95de29b4dd7535d16431c32c2fbbbd146bfd0d45
Fauziah <uzziifz7@gmail.com>
2024-10-10 19:27:05 +0800

    Operator penugasan : Pengisian dan Modulo

---
+++ Day39.java
@@ -0,0 +1,13 @@
+public class day_thirtynine_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator penugasan");
+        byte a = 9;
+        byte b = 5;
+        
+        // pengisian dan modulo
+        a %= b;
+        System.out.println("Sisa bagi variable a dan b : " + a);
+    }
+    
+}
