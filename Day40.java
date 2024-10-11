507e49138e4a7bb62a6db0df8e66f7d8e52fd6e2
Fauziah <uzziifz7@gmail.com>
2024-10-11 18:38:58 +0800

    Operator Perbandingan : Lebih Besar dan Lebih Kecil

---
+++ Day40.java
@@ -0,0 +1,14 @@
+public class day_forty_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator perbandingan");
+        int a = 50;
+        int b = 5;
+        
+        // Lebih kecil (<)
+        System.out.println(a < b);
+        // Lebih besar (>)
+        System.out.println(a > b);
+    }
+    
+}
