c145e90f84d20bed903461972700956f79c99909
Fauziah <uzziifz7@gmail.com>
2024-10-13 19:07:12 +0800

    Operator Perbandingan : Sama Dengan dan Tidak Sama Dengan

---
+++ Day42.java
@@ -0,0 +1,14 @@
+public class day_fortytwo_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator Perbandingan");
+        byte h = 8;
+        byte i = 2;
+        
+        // Sama dengan (=)
+        System.out.println(h == i);
+        // Tidak Sama dengan (!=)
+        System.out.println(h != i);
+    }
+    
+}
