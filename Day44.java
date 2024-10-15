bfe62715fc1d9510d445d4fb66f4a02b2fcd1de8
Fauziah <uzziifz7@gmail.com>
2024-10-15 18:43:29 +0800

    Operator Logika : AND

---
+++ Day44.java
@@ -0,0 +1,17 @@
+public class day_fortyfour_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator Logika");
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+        boolean d = false;
+        
+        // AND(&&)
+        System.out.println(a && b);
+        System.out.println(a && d);
+        System.out.println(c && b);
+        System.out.println(c && d);
+    }
+    
+}
