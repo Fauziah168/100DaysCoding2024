b82b74cf33ec38a668e480d8acbaea5a44aa13bb
Fauziah <uzziifz7@gmail.com>
2024-10-16 19:10:44 +0800

    Operator Logika : OR

---
+++ Day45.java
@@ -0,0 +1,18 @@
+public class day_fortyfive_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator Logika");
+        boolean a, b, c, d;
+        a = true;
+        b = true;
+        c = false;
+        d = false;
+        
+        // OR ( || )
+        System.out.println(a || b);
+        System.out.println(a || c);
+        System.out.println(d || b);
+        System.out.println(c || d);
+    }
+    
+}
