d5d0a7081a998b81c071642777224a9c55b3d978
Fauziah <uzziifz7@gmail.com>
2024-10-17 19:19:29 +0800

    Operator Logika : Negasi / Kebalikan

---
+++ Day46.java
@@ -0,0 +1,13 @@
+public class day_fortysix_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Operator Logika");
+        boolean a = true;
+        boolean b = false;
+        
+        // negasi (!)
+        System.out.println(!b);
+        System.out.println(!a);
+    }
+    
+}
