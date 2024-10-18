291902b8c80508fddd7f261c4ee551277c585eb9
Fauziah <uzziifz7@gmail.com>
2024-10-18 18:52:28 +0800

    Percabangan : if

---
+++ Day47.java
@@ -0,0 +1,19 @@
+public class day_fortyseven_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Percabanga");
+        byte a = 3;
+        byte b = 5;
+        
+        if (a == 3) {
+            System.out.println("a : 3 benar");
+        } 
+        if (b == 4) {
+            System.out.println("b : 4 benar");
+        }
+        if (a < b) {
+            System.out.println("a lebih kecil dari pada b");
+        }
+    }
+    
+}
