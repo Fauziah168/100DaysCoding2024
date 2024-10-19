21a2c0145a93d9c5c20c2c836d063e44434d672e
Fauziah <uzziifz7@gmail.com>
2024-10-19 20:31:25 +0800

    Percabangan : if - else

---
+++ Day48.java
@@ -0,0 +1,14 @@
+public class day_fortyeight_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Percabangan");
+        int a = 5;
+        
+        if (a >= 8) {
+            System.out.println("pernyataan benar");
+        } else {
+            System.out.println("pernyataan salah");
+        }
+    }
+    
+}
