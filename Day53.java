bdd12eac52d5e9e2827e9f7a857214f49c2ac310
Fauziah <uzziifz7@gmail.com>
2024-10-24 01:21:18 +0800

    Looping : For

---
+++ Day53.java
@@ -0,0 +1,11 @@
+public class day_fiftythree_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Looping");
+        
+        for (int angka = 1; angka <= 5; angka++) {
+            System.out.println("Angka : " + angka);
+        }
+    }
+    
+}
