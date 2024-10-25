c84207cab01a75c9923239b83fe52ee7cd5db6a1
Fauziah <uzziifz7@gmail.com>
2024-10-25 20:25:21 +0800

    Looping : For Increment

---
+++ Day54.java
@@ -0,0 +1,11 @@
+public class day_fiftyfour_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Looping");
+        
+        for (int a = 1; a <= 50; a += 2) {
+            System.out.println(a);
+        }
+    }
+    
+}
