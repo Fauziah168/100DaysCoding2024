50f7bbeced7952c884fddd6d7c5b749daf6051e3
Fauziah <uzziifz7@gmail.com>
2024-10-26 20:48:09 +0800

    Looping : For Decrement

---
+++ Day55.java
@@ -0,0 +1,11 @@
+public class day_fiftyfive_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("looping");
+        
+        for (int apa = 15; apa > 0; apa--) {
+            System.out.println(apa);
+        }
+    }
+    
+}
