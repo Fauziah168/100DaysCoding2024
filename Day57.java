cd422639c7523b8f6d050a63b74cdb5444524cb0
Fauziah <uzziifz7@gmail.com>
2024-10-28 22:01:19 +0800

    Looping : While Increment

---
+++ Day57.java
@@ -0,0 +1,13 @@
+public class day_fiftyseven_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("looping");
+        int var = 0;
+        
+        while (var <= 15) {
+            System.out.println("apel");
+            var++;
+        }
+    }
+    
+}
