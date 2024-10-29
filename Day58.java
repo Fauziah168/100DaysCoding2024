24973e9868eeec14525712e7f37057536dfb19b3
Fauziah <uzziifz7@gmail.com>
2024-10-29 20:20:06 +0800

    Looping : While Decrement

---
+++ Day58.java
@@ -0,0 +1,13 @@
+public class day_fiftyeight_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("looping");
+        short fauziah = 20;
+        
+        while (fauziah >= 1) {
+            System.out.println("angka" + fauziah);
+            fauziah--;
+        }
+    }
+    
+}
