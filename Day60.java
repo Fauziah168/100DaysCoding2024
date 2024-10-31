a1c61dcd5e16a501a19705efec6ce4f83dc85c93
Fauziah <uzziifz7@gmail.com>
2024-10-31 19:23:40 +0800

    Looping : Do While

---
+++ Day60.java
@@ -0,0 +1,15 @@
+public class day_sixty_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("Looping");
+        int a = 1;
+        
+        do {
+            System.out.println("selama malam");
+            System.out.println("semoga bahagia");
+            System.out.println(" ");
+            a++;
+        } while (a <= 5);
+    }
+    
+}
