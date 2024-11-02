19ee45c4b8d30cc674a5d530e46a726031ed2463
Fauziah <uzziifz7@gmail.com>
2024-11-02 21:06:03 +0800

    Keyword : Break

---
+++ Day62.java
@@ -0,0 +1,13 @@
+public class day_sixtytwo_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("keyword");
+        for (int a = 0; a < 10; a++) {
+            if (a == 5) {
+                break; // Loop akan berhenti jika mencapai 5
+            }
+            System.out.println("angka : " + a);
+        }
+        System.out.println("Loop selesai.");
+    }
+}
