7639314c96ead404fd6c7c7105b724e5c0d2962c
Fauziah <uzziifz7@gmail.com>
2024-11-03 20:08:52 +0800

    Keyword : Continue

---
+++ Day63.java
@@ -0,0 +1,13 @@
+public class day_sixtythree_coding {
+    
+    public static void main(String[] args) {
+        System.out.println("keyword");
+        for (byte a = 1; a <= 20; a++) {
+            if(a %2 == 0) {
+                continue;
+            }
+            System.out.println("angka : " + a);
+        }
+    }
+    
+}
