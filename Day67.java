d2c170888f86fd423c87e54eca5b5ebd011957b4
Fauziah <uzziifz7@gmail.com>
2024-11-07 21:46:55 +0800

    Method Void

---
+++ Day67.java
@@ -0,0 +1,17 @@
+public class day_sixtyseven_coding {
+    public void cetakSalam() {
+        System.out.println("Halo! Selamat datang");
+    }
+    
+    public static void main(String[] args) {
+        day_sixtyseven_coding contoh = new day_sixtyseven_coding();
+        contoh.cetakSalam();
+    }
+}
+/*Method void berguna untuk operasi
+  atau tugas yang tidak memerlukan
+  pengembalian nilai. Ini membantu 
+  menyederhanakan kode saat output dari 
+  suatu method tidak diperlukan, dan 
+  cukup melakukan tugas tertentu tanpa 
+  interaksi lebih lanjut dengan nilai hasil.*/
