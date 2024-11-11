c7f54a8b47e73e27f35730096a519dd69267c875
Fauziah <uzziifz7@gmail.com>
2024-11-11 22:23:04 +0800

    Method Static

---
+++ Day71.java
@@ -0,0 +1,10 @@
+public class day_seventyone_coding {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int hasil = day_seventyone_coding.add(5, 10);
+        System.out.println("Hasil : " + hasil);
+    }
+    
+}
