b9550012f11c0a6bb3f0dc88b196e9dd3c13bff8
Fauziah <uzziifz7@gmail.com>
2024-11-13 23:08:21 +0800

    String Method : charAt()

---
+++ Day73.java
@@ -0,0 +1,11 @@
+public class day_seventythree_coding {
+    
+    public static void main(String[] args) {
+        String teks = "Hallo nama ku Fauziah";
+        System.out.println(teks);
+        
+        char karakter = teks.charAt(8);
+        System.out.println("Karakter pada indeks ke-8 adalah " + karakter);
+    }
+    
+}
