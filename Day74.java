79cc534528857c6d984b0a028e2aec9fa3fa855b
Fauziah <uzziifz7@gmail.com>
2024-11-14 20:22:17 +0800

    String Method : length()

---
+++ Day74.java
@@ -0,0 +1,8 @@
+public class day_seventyfour_coding {
+    
+    public static void main(String[] args) {
+        String kata = "hay semuanya! selamat malam";
+        int panjang = kata.length();
+        System.out.println("Panjang dari string '" + kata + "' adalah: " + panjang);
+    }
+}
