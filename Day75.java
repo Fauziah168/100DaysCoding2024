53452d96d0c88ba834ae90547d0db5e778362746
Fauziah <uzziifz7@gmail.com>
2024-11-15 21:42:36 +0800

    String Method : toLowerCase()

---
+++ Day75.java
@@ -0,0 +1,9 @@
+public class day_seventyfive_coding {
+    
+    public static void main(String[] args) {
+        String text = "SELAMAT MALAM!";
+        String huruf_kecil = text.toLowerCase();
+        
+        System.out.println(huruf_kecil); 
+    }
+}
