22b14491b3bab69cc9a469db12e673a8e8d6f060
Fauziah <uzziifz7@gmail.com>
2024-11-16 20:39:34 +0800

    String Method : toUpperCase()

---
+++ Day76.java
@@ -0,0 +1,12 @@
+public class day_seventysix_coding {
+    
+    public static void main(String[] args) {
+        String teks = "selamat malam!";
+        String hasil = teks.toUpperCase();
+        System.out.println(hasil);
+        
+        String nama = "Perkenalkan Nama Saya Fauziah";
+        String namaKapital = nama.toUpperCase();
+        System.out.println(namaKapital);
+    }
+}
