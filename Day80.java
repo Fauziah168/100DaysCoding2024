354890dfdd58c96c656aca19af56f445efbfe206
Fauziah <uzziifz7@gmail.com>
2024-11-20 20:50:11 +0800

    String Method : trim()

---
+++ Day80.java
@@ -0,0 +1,11 @@
+public class day_eighty_coding {
+    
+    public static void main(String[] args) {
+        String a = "  Selamat malam  ";
+        String b = a.trim();
+        
+        System.out.println("Sebelum : " + a);
+        System.out.println("Setelah : " + b);
+    }
+    
+}
