8c19741f806f4a07de60f49d6b1a443835780b4c
Fauziah <uzziifz7@gmail.com>
2024-11-21 20:31:28 +0800

    String Method : valueOf()

---
+++ Day81.java
@@ -0,0 +1,16 @@
+public class day_eightyone_coding {
+    
+    public static void main(String[] args) {
+        int a = 42;
+        double b = 3.14;
+        boolean c = true;
+
+        String varA = String.valueOf(a);
+        String varB = String.valueOf(b);
+        String varC = String.valueOf(c);
+
+        System.out.println("Integer ke String : " + varA);
+        System.out.println("Double ke String : " + varB);
+        System.out.println("Boolean ke String : " + varC);
+    }
+}
