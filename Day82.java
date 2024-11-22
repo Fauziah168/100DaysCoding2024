149ab1390057c53c9c180eaf15946fc2101a67b5
Fauziah <uzziifz7@gmail.com>
2024-11-22 21:17:23 +0800

    Method ceil(), floor(), dan round()

---
+++ Day82.java
@@ -0,0 +1,17 @@
+public class day_eightytwo_coding {
+    
+    public static void main(String[] args) {
+        double a = 3.6;
+        
+        double ceil = Math.ceil(a);
+        System.out.println("Hasil dari ceil : " + ceil);
+        double floor = Math.floor(a);
+        System.out.println("Hasil dari floor : " + floor);
+        double round = Math.round(a);
+        System.out.println("Hasil dari round : " + round);
+        
+        double aNegatif = -3.6;
+            System.out.println("Hasil dari Ceil dengan angka negatif : " + Math.ceil(aNegatif));
+    }
+    
+}
