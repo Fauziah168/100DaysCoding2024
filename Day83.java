9182c0d06e7a256e8acebc995ccb6313bac4d514
Fauziah <uzziifz7@gmail.com>
2024-11-23 21:49:52 +0800

    Membuat Objek Array Satu Dimensi

---
+++ Day83.java
@@ -0,0 +1,15 @@
+public class day_eightythree_codong {
+    
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[0] = 5;
+        a[1] = 15;
+        a[2] = 25;
+        
+        System.out.println("a[0] : " + a[0]);
+        System.out.println("a[1] : " + a[1]);
+        System.out.println("a[2] : " + a[2]);
+    }
+    
+    
+}
