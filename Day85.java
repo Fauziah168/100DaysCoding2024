9a220df949d31075028ddad94e0d31c70c9efbcb
Fauziah <uzziifz7@gmail.com>
2024-11-25 22:34:13 +0800

    Mengisi Element ke Array Satu Dimensi Pada Saat Inisialisasi

---
+++ Day85.java
@@ -0,0 +1,18 @@
+public class day_eightyfive_coding {
+    
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+        
+        System.out.print(a[0]);
+        System.out.print(a[1]);
+        System.out.print(a[2]);
+        System.out.print(a[3]);
+        System.out.print(a[4]);
+    }
+}
