ea0f5e963e13310dd9616f234ccde9a081139caa
Fauziah <uzziifz7@gmail.com>
2024-11-26 21:20:58 +0800

    Mencetak Elemen Pada Array Satu Dimensi

---
+++ Day86.java
@@ -0,0 +1,14 @@
+public class day_eightynine_coding {
+    
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        
+        int b = 0;
+
+        System.out.println("Elemen pada array:");
+        while (b < a.length) {
+            System.out.println(a[b]);
+            b++;
+        }
+    }
+}
